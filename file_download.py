a73e1cbdbdabb3f0b8a88994652e7c20d51c3e10
Andrew Taylor <andrew_t97@hotmail.co.uk>
2019-03-13 16:08:04 +0000

    Added functionality to check for upcoming bookings and download files for them

--- file_download.py
+++ file_download.py
@@ -2,15 +2,19 @@
 import sys
 import subprocess
 import requests
+import datetime
+from datetime import timedelta
 
 host = 'http://sds.samchatfield.com'
-url = host + '/api/user/1234567/files'
 
-def download_files():
-    #Fetching the json of the files in the current directory
-    getReq = requests.get(url = url)
-    data = getReq.json()
+def download_files(usr_id):
     
+    file_url = host + '/api/user/' + usr_id + '/files'
+
+    #Fetching the json of the files in the current directory
+    getReq = requests.get(url = file_url)
+    data = getReq.json()
+
     #Searching file directory for pdfs and writing them to a temp file
     #to be opened in chrome
     for x in data:
@@ -28,4 +32,25 @@ def download_files():
             process = subprocess.Popen(command, shell = False)
     return
 
-download_files()
+def check_booking():
+    rm_command = ['rm', 'temp/*']
+    current_dt = datetime.datetime.now()
+    nxt_booking = current_dt.hour + 1
+    book_url = host + '/api/room/CS-225/bookings/' + current_dt.strftime('%Y-%m-%d') + 'T' + str(nxt_booking) + ':00Z'
+    getReq = requests.get(url = book_url)
+    data = getReq.json()
+    
+    if (data is None):
+        print('>>> No next booking')
+        return
+        
+    while(true):
+        if (data.datetime.now() != data['start']):
+            break
+
+    process = subprocess.Popen(rm_command, shell = False)
+    download_files(data['leader'])
+
+    return
+
+check_booking()
